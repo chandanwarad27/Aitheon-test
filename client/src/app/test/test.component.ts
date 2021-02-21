f5acd1a3bc8b390476a00c44ccec805116ef85ab
chandanwarad27 <chandan.warad@gmail.com>
2021-02-21 20:41:15 +0530

    Added functionality to fetch questions from JSON and render it on screen.

--- client/src/app/test/test.component.ts
+++ client/src/app/test/test.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgModule } from "@angular/core";
+import { QuizService } from "../services/quiz.service";
+import { Question, Quiz, Option } from "../models/index";
 
 @Component({
-  selector: 'app-test',
-  templateUrl: './test.component.html',
-  styleUrls: ['./test.component.scss']
+  selector: "app-test",
+  templateUrl: "./test.component.html",
+  styleUrls: ["./test.component.scss"],
+  providers: [QuizService],
 })
 export class TestComponent implements OnInit {
+  quiz: Quiz = new Quiz(null);
+  mode = "quiz";
+  quizName: string;
+  pager = {
+    index: 0,
+    size: 1,
+    count: 1,
+  };
 
-  constructor() { }
+  constructor(private quizService: QuizService) {}
 
   ngOnInit() {
+    this.quizName = this.quizService.getPath();
+    this.loadQuiz(this.quizName);
   }
 
+  loadQuiz(quizName: string) {
+    this.quizService.get(quizName).subscribe((res) => {
+      this.quiz = new Quiz(res);
+      this.pager.count = this.quiz.questions.length;
+    });
+    this.mode = "quiz";
+  }
+
+  get filteredQuestions() {
+    return this.quiz.questions
+      ? this.quiz.questions.slice(
+          this.pager.index,
+          this.pager.index + this.pager.size
+        )
+      : [];
+  }
 }
